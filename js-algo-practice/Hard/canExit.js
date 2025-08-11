5394d30f94adbd3a661195c074706cdc0a690c4e
ordamri <ordamry19@gmail.com>
2025-08-11 14:45:45 +0300

    Added solution to canExit algo question

--- js-algo-practice/Hard/canExit.js
+++ js-algo-practice/Hard/canExit.js
@@ -44,8 +44,44 @@ In a maze of size m x n, you enter at [0, 0] and exit at [m-1, n-1].
 There can be dead ends in a maze - one exit path is sufficient.
 */
 
-function canExit( /*args*/ ) {
-  //your code
+function canExit( maze ) {
+  const rows = maze.length
+  const cols = maze[0].length
+
+  if (maze[0][0] === 1 || maze[rows-1][cols-1] === 1) return false
+
+  const directions = [
+    [1, 0], // down
+    [-1, 0], //up
+    [0, 1], // right
+    [0, -1] // left
+  ]
+
+  const queue = [[0,0]]
+
+  const visited = [...Array(rows)].map(() => Array(cols).fill(false));
+  visited[0][0] = true
+  
+  while (queue.length > 0){
+    const [r, c] = queue.shift()
+
+    if (r === rows - 1 && c === cols -1) return true
+
+    for (const [dr, dc] of directions){
+      const newRow = r + dr
+      const newCol = c + dc
+
+      if (
+        newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && maze [newRow][newCol] === 0 && !visited[newRow][newCol]
+      ) {
+        visited[newRow][newCol] = true
+        queue.push([newRow, newCol])
+      }
+    }
+  }
+
+  return false ;
+  
 }
 
 exports.solution = canExit;
